70675e6057f0d9cca1eb99780c48804a028422c1
Philip Schmoekel <philip.schmoekel@googlemail.com>
2017-07-15 11:27:44 +0200

    handling single days without meals

--- core/menus/studierendenwerk-adapter/parse-site.js
+++ core/menus/studierendenwerk-adapter/parse-site.js
@@ -6,29 +6,33 @@ module.exports = async function (menuConfig) {
   try {
     const daySections = html.match(/<section class="fmc-day.*">([\s\S]*?)<\/section>/g);
     const days = daySections.map(dayHtml => {
-      const headParsed = dayHtml
-        .match(/class="fmc-head">([\s\S^]*?)<span class="light">([\s\S]*?)<\/span>/);
-      const itemsParsed = dayHtml
-        .match(/ul class="fmc-items">([\s\S]*?)<\/ul>/)[1]
-        .match(/<li class="fmc-item".*?>([\s\S]*?)<\/li>/g)
-        .map(itemParsed => {
-          return {
-            title: parseText(itemParsed.match(/span class="fmc-item-title">([\s\S]*?)<\/span>/)[1]),
-            location: parseText(itemParsed.match(/span class="fmc-item-location">([\s\S]*?)<\/span>/)[1]),
-            price: parseText(itemParsed.match(/span class="fmc-item-price">([\s\S]*?)<\/span>/)[1])
-          }
-        });
+      try {
+        const headParsed = dayHtml
+          .match(/class="fmc-head">([\s\S^]*?)<span class="light">([\s\S]*?)<\/span>/);
+        const itemsParsed = dayHtml
+          .match(/ul class="fmc-items">([\s\S]*?)<\/ul>/)[1]
+          .match(/<li class="fmc-item".*?>([\s\S]*?)<\/li>/g)
+          .map(itemParsed => {
+            return {
+              title: parseText(itemParsed.match(/span class="fmc-item-title">([\s\S]*?)<\/span>/)[1]),
+              location: parseText(itemParsed.match(/span class="fmc-item-location">([\s\S]*?)<\/span>/)[1]),
+              price: parseText(itemParsed.match(/span class="fmc-item-price">([\s\S]*?)<\/span>/)[1])
+            }
+          });
 
-      return {
-        dayOfWeek: parseText(headParsed[1]),
-        date: parseText(headParsed[2]),
-        items: itemsParsed
-      };
+        return {
+          dayOfWeek: parseText(headParsed[1]),
+          date: parseText(headParsed[2]),
+          items: itemsParsed
+        };
+      } catch (_) {
+        return null;
+      }
     });
 
     return {
       name: menuConfig.name,
-      days: days
+      days: days.filter(i => !!i)
     }
   }
   catch(e) {
